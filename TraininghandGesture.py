55bd6630e6d64997c3fac4e85f0fbd093eb42ff3
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-11-18 17:01:24 +0530

    3

--- TraininghandGesture.py
+++ TraininghandGesture.py
@@ -1,14 +1,17 @@
+### 01.Importing libraries
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
-from keras.layers import Dropout
+from keras.layers import Dropout ## overfiting prevent 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+##########################################################################
 
-model=Sequential()
-model.add(Conv2D(32,(3,3),input_shape=(256,256,1),activation='relu'))
+### 02.Design CNN model
+model=Sequential() #oreder of layers about the model
+model.add(Conv2D(32,(3,3),input_shape=(256,256,1),activation='relu'))#input layer
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Conv2D(64,(3,3),activation='relu'))
@@ -21,11 +24,13 @@ model.add(Conv2D(256,(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 
-model.add(Dense(units=150,activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(units=6,activation='softmax'))
-model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.add(Dense(units=150,activation='relu')) #fully connected layer
+model.add(Dropout(0.25))# overfitting prevent
+model.add(Dense(units=6,activation='softmax')) # Output layer-6 classes
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+##########################################################################
 
+### 03.Pre-processing data
 train_datagen=ImageDataGenerator(rescale=1./255,
                                  rotation_range=12,
                                  width_shift_range=0.2,
@@ -37,7 +42,8 @@ val_datagen=ImageDataGenerator(rescale=1./255)
 train_generator=train_datagen.flow_from_directory('HandGestureDataset/train',
                                                  target_size=(256,256),
                                                  color_mode='grayscale',
-                                                 batch_size=32,
+                                                 batch_size=8,
+                                                 classes=['NONE','ONE','TWO','THREE','FOUR','FIVE'],
                                                  class_mode='categorical')
 
 val_generator=val_datagen.flow_from_directory('HandGestureDataset/test',
@@ -57,7 +63,7 @@ model.fit(train_generator,
                     validation_data=val_generator,
                     validation_steps=7,
                     callbacks=callback_list 
-                    )
+         )
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
